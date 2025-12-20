f0e58545822f4b3aaf00e41d39572617c7d5529f
Murilo Monte <105597129+murilomonte@users.noreply.github.com>
2025-12-20 09:28:49 -0300

    feat: initial setup

---
+++ eslint.config.js
@@ -0,0 +1,41 @@
+import js from "@eslint/js";
+import globals from "globals";
+import reactHooks from "eslint-plugin-react-hooks";
+import reactRefresh from "eslint-plugin-react-refresh";
+import { defineConfig, globalIgnores } from "eslint/config";
+
+export default defineConfig([
+  globalIgnores(["dist"]),
+  {
+    env: {
+      browser: true,
+    },
+    extends: [
+      "eslint:recommended",
+      "plugin:react/recommended",
+      "plugin:react/jsx-runtime",
+      "plugin:react-hooks/recommended",
+    ],
+    parserOptions: {
+      ecmaVersion: "latest",
+      sourceType: "module",
+      ecmaFeatures: {
+        jsx: true,
+      },
+    },
+    settings: {
+      react: {
+        version: "detect",
+      },
+    },
+    plugins: ["react-refresh"],
+    rules: {
+      "react-refresh/only-export-components": "off",
+      "react/react-in-jsx-scope": "off",
+      "react/prop-types": "off",
+      "no-unsafe-finally": "off",
+      "no-unused-vars": "off",
+      "react/jsx-key": "off",
+    },
+  },
+]);
---
+++ src/Hooks/useFetch.tsx
@@ -0,0 +1,65 @@
+import React from "react";
+
+interface FetchState<T> {
+  data: T | null;
+  loading: boolean;
+  error: string | null;
+}
+
+const useFetch = <T,>(
+  url: RequestInfo | URL,
+  options?: RequestInit,
+): FetchState<T> => {
+  const [data, setData] = React.useState<T | null>(null);
+  const [loading, setLoading] = React.useState(false);
+  const [error, setError] = React.useState<string | null>(null);
+
+  const abortControllerRef = React.useRef<AbortController | null>(null);
+  const optionsRef = React.useRef(options);
+  optionsRef.current = options;
+  
+  React.useEffect(() => {
+    const abortController = new AbortController();
+    abortControllerRef.current = abortController;
+
+    async function request() {
+      setLoading(true);
+      setError(null);
+      setData(null);
+
+      let response: Response | null;
+      let json: T | null;
+
+      try {
+        response = await fetch(url, {
+          ...optionsRef.current,
+          signal: abortController.signal,
+        });
+        if (!response.ok) throw new Error("Error: " + response.status);
+        json = await response.json();
+        if (!abortController.signal.aborted) setData(json);
+      } catch (err) {
+        if (!abortController.signal.aborted && err instanceof Error)
+          setError(err.message);
+        setData(null);
+        response = null;
+        json = null;
+      } finally {
+        setLoading(false);
+      }
+    }
+    request();
+
+    return () => {
+      abortController.abort();
+    };
+  }, [url]);
+
+  return {
+    data,
+    loading,
+    error,
+  };
+};
+
+export default useFetch;
