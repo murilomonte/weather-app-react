eacea7c8adf37c3b369090a4a3682116296c92df
Murilo Monte <105597129+murilomonte@users.noreply.github.com>
2025-12-26 16:14:27 -0300

    feat: hourly forecast done

--- src/Components/Weather/HourlyForecast.tsx
+++ src/Components/Weather/HourlyForecast.tsx
@@ -1,9 +1,123 @@
-import React from 'react'
+import React from "react";
+import { useWeather } from "../../Context/WeatherContext";
+import { extractDayHour } from "../../utils/strToDate";
+import { WeatherIcon } from "./WeatherIcon";
+import styles from "./HourlyForecast.module.css";
+import DownArrowSVG from "../../assets/images/icon-dropdown.svg?react";
+
+type HourlyWeather = {
+  day: string;
+  hour: number;
+  temp: number;
+  code: number;
+};
 
 const HourlyForecast = () => {
-  return (
-    <div>HourlyForecast</div>
-  )
-}
+  const { data, loading } = useWeather();
+  const [forecast, setForecast] = React.useState<HourlyWeather[]>();
+  const [activeForecast, setActiveForecast] = React.useState<string>();
+  const [dropdown, setdropdown] = React.useState(false);
 
-export default HourlyForecast
+  React.useEffect(() => {
+    if (!data) return;
+    const hourly = data.hourly;
+
+    let hourlyList: HourlyWeather[] = [];
+
+    hourly.time.map((_, i) => {
+      const { hour, day } = extractDayHour(hourly.time[i]);
+      hourlyList.push({
+        day: day,
+        hour: hour,
+        temp: hourly.temperature_2m[i],
+        code: hourly.weather_code[i],
+      });
+    });
+
+    setForecast(hourlyList);
+    setActiveForecast(hourlyList[0].day);
+  }, [data]);
+
+  React.useEffect(() => {
+    if (dropdown) {
+      
+    }
+  }, [dropdown])
+
+  function uniqueDays(list: HourlyWeather[]): { day: string }[] {
+    return Array.from(new Map(list.map((item) => [item.day, item])).values());
+  }
+
+  if (loading)
+    return (
+      <section className={styles.hourlyForecastContainer}>
+        <div className={styles.forecastHeader}>
+          <h2>Hourly forecast</h2>
+          <div className={styles.forecastDropdownWrapper}>
+            <button>-  <DownArrowSVG /></button>
+          </div>
+        </div>
+        <div className={styles.hourlyForecast}>
+          <div className={styles.forecastLoading}></div>
+          <div className={styles.forecastLoading}></div>
+          <div className={styles.forecastLoading}></div>
+          <div className={styles.forecastLoading}></div>
+          <div className={styles.forecastLoading}></div>
+          <div className={styles.forecastLoading}></div>
+        </div>
+      </section>
+    );
+  if (forecast)
+    return (
+      <section className={styles.hourlyForecastContainer}>
+        <div className={styles.forecastHeader}>
+          <h2>Hourly forecast</h2>
+          <div className={styles.forecastDropdownWrapper}>
+            <button onClick={() => setdropdown((prev) => !prev)}>
+              {activeForecast}
+              <DownArrowSVG />
+            </button>
+            <div
+              className={`${styles.dropdown} ${dropdown ? styles.active : ""}`}
+            >
+              {uniqueDays(forecast).map((item) => {
+                return (
+                  <button
+                    onClick={() => {
+                      setActiveForecast(item.day);
+                      setdropdown(false);
+                    }}
+                    key={item.day}
+                  >
+                    {item.day}
+                  </button>
+                );
+              })}
+            </div>
+          </div>
+        </div>
+        <div className={styles.hourlyForecast}>
+          {forecast
+            ?.filter((item) => {
+              return item.day == activeForecast;
+            })
+            .map((item) => {
+              return (
+                <div key={item.hour} className={styles.forecast}>
+                  <div>
+                    <WeatherIcon wmoCode={item.code} loading="lazy" />
+                    {item.hour > 12
+                      ? item.hour - 12 + " PM"
+                      : item.hour + " AM"}
+                  </div>
+                  <p className={styles.temp}>{Math.round(item.temp)}</p>
+                </div>
+              );
+            })}
+        </div>
+      </section>
+    );
+  return null;
+};
+
+export default HourlyForecast;
